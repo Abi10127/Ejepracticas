a620cb33cc71d37a5418b1c6e4cdaef349c276ef
Michin <ubaldoabi9@gmail.com>
2026-05-22 04:26:55 -0600

    Create otrofor.c

---
+++ otrofor.c
@@ -0,0 +1,13 @@
+//código de for que no sé todavía qué es lo que va a gacer pero tengo que praticar aunque sea uno
+
+#include <stdio.h>
+int main()
+{
+  for(int i = 0; i < 6; i++)
+  {
+    printf("La suma de los 6 números iniciando desde 0\n");
+    printf("%d\n", i);
+  }
+  return 0;
+}
+    
