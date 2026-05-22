14fdfa23f403d62298ecd628ea32ca8b62540b08
Michin <ubaldoabi9@gmail.com>
2026-05-22 04:19:27 -0600

    Create forejemplo.c

---
+++ forejemplo.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() 
+{
+    for (int i = 1; i <= 5; i++) 
+    {
+        printf("Vuelta número: %d\n", i);
+    }
+    
+    printf("¡Ciclo terminado!\n");
+    return 0;
+}
