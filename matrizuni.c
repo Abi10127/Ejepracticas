0250dc4611e00111de9a468a3c0e5c277f6e5d26
Michin <ubaldoabi9@gmail.com>
2026-05-22 04:45:31 -0600

    Create matrizuni.c

---
+++ matrizuni.c
@@ -0,0 +1,16 @@
+//intento de creación de un programa que muestra un arreglo unidimensional 
+
+#include <stdio.h>
+int main()
+{
+  int matriz[4] = {0, 1 , 2, 7};
+  int i = 0;
+
+  while (i < 4)
+  {
+    printf("El valor %d de la matriz es %d\n", i+1, matriz[i]);
+    i = i + 1;
+  }
+  printf("NCT 127 mention")
+  return 0;
+}
